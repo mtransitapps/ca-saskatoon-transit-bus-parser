7f1343a0b3d869293a97ce39a00ff33655ab37f2
Mathieu Méa <mathieu.mea@gmail.com>
2021-03-19 11:30:30 -0400

    March 19 update

--- src/main/java/org/mtransit/parser/ca_saskatoon_transit_bus/SaskatoonTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_saskatoon_transit_bus/SaskatoonTransitBusAgencyTools.java
@@ -3,20 +3,14 @@ package org.mtransit.parser.ca_saskatoon_transit_bus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
+import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -25,47 +19,19 @@ import java.util.regex.Pattern;
 // http://apps2.saskatoon.ca/app/data/google_transit.zip
 public class SaskatoonTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-saskatoon-transit-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new SaskatoonTransitBusAgencyTools().start(args);
 	}
 
-	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
+	@NotNull
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating Saskatoon Transit bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating Saskatoon Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public String getAgencyName() {
+		return "Saskatoon Transit";
 	}
 
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
-	}
-
-	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
 	@Override
@@ -75,9 +41,6 @@ public class SaskatoonTransitBusAgencyTools extends DefaultAgencyTools {
 		}
 		if ("Out Of Service".equalsIgnoreCase(gTrip.getTripHeadsign())) {
 			return true;
-		}
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
 		}
 		return super.excludeTrip(gTrip);
 	}
@@ -108,7 +71,7 @@ public class SaskatoonTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@NotNull
 	@Override
-	public String cleanRouteLongName(String routeLongName) {
+	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, routeLongName);
 		return CleanUtils.cleanLabel(routeLongName);
 	}
@@ -130,105 +93,108 @@ public class SaskatoonTransitBusAgencyTools extends DefaultAgencyTools {
 	@Nullable
 	@Override
 	public String getRouteColor(@NotNull GRoute gRoute) {
-		int rsn = Integer.parseInt(gRoute.getRouteShortName().trim());
-		switch (rsn) {
-		// @formatter:off
-		case 1: return null;
-		case 2: return null;
-		case 3: return null;
-		case 4: return null;
-		case 5: return null;
-		case 6: return null;
-		case 7: return null;
-		case 8: return null;
-		case 9: return null;
-		case 10: return null;
-		case 11: return null;
-		case 12: return null;
-		case 13: return null;
-		case 14: return null;
-		case 15: return null;
-		case 17: return null;
-		case 18: return null;
-		case 19: return null;
-		case 20: return null;
-		case 21: return null;
-		case 22: return null;
-		case 23: return null;
-		case 25: return null;
-		case 26: return null;
-		case 27: return null;
-		case 28: return null;
-		case 29: return null;
-		case 30: return null;
-		case 35: return null;
-		case 40: return null;
-		case 44: return null;
-		case 43: return null;
-		case 45: return null;
-		case 50: return COLOR_DART_GREEN;
-		case 55: return null;
-		case 60: return COLOR_DART_GREEN;
-		case 61: return null;
-		case 62: return null;
-		case 63: return null;
-		case 64: return null;
-		case 65: return null;
-		case 70: return COLOR_DART_GREEN;
-		case 75: return null;
-		case 80: return COLOR_DART_GREEN;
-		case 81: return null;
-		case 82: return null;
-		case 83: return null;
-		case 84: return null;
-		case 85: return null;
-		case 86: return null;
-		//
-		case 101: return SCHOOL_BUS_COLOR;
-		case 102: return SCHOOL_BUS_COLOR;
-		case 104: return SCHOOL_BUS_COLOR;
-		case 117: return SCHOOL_BUS_COLOR;
-		case 150: return SCHOOL_BUS_COLOR;
-		case 151: return SCHOOL_BUS_COLOR;
-		case 152: return SCHOOL_BUS_COLOR;
-		case 153: return SCHOOL_BUS_COLOR;
-		case 180: return SCHOOL_BUS_COLOR;
-		//
-		case 305: return SCHOOL_BUS_COLOR;
-		case 311: return SCHOOL_BUS_COLOR;
-		case 314: return SCHOOL_BUS_COLOR;
-		case 315: return SCHOOL_BUS_COLOR;
-		case 316: return SCHOOL_BUS_COLOR;
-		case 322: return SCHOOL_BUS_COLOR;
-		case 325: return SCHOOL_BUS_COLOR;
-		case 331: return SCHOOL_BUS_COLOR;
-		case 332: return SCHOOL_BUS_COLOR;
-		case 333: return SCHOOL_BUS_COLOR;
-		case 334: return SCHOOL_BUS_COLOR;
-		case 335: return SCHOOL_BUS_COLOR;
-		case 336: return SCHOOL_BUS_COLOR;
-		case 338: return SCHOOL_BUS_COLOR;
-		case 339: return SCHOOL_BUS_COLOR;
-		case 341: return SCHOOL_BUS_COLOR;
-		case 342: return SCHOOL_BUS_COLOR;
-		case 346: return SCHOOL_BUS_COLOR;
-		case 348: return SCHOOL_BUS_COLOR;
-		case 349: return SCHOOL_BUS_COLOR;
-		case 352: return SCHOOL_BUS_COLOR;
-		case 354: return SCHOOL_BUS_COLOR;
-		case 356: return SCHOOL_BUS_COLOR;
-		case 358: return SCHOOL_BUS_COLOR;
-		//
-		case 514: return null;
-		case 523: return null;
-		//
-		case 808: return null;
-		//
-		case 1225: return null;
-		// @formatter:on
-		default:
-			throw new MTLog.Fatal("Unexpected route color %s!", gRoute.toStringPlus());
+		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
+			final int rsn = Integer.parseInt(gRoute.getRouteShortName().trim());
+			switch (rsn) {
+			// @formatter:off
+			case 1: return null;
+			case 2: return null;
+			case 3: return null;
+			case 4: return null;
+			case 5: return null;
+			case 6: return null;
+			case 7: return null;
+			case 8: return null;
+			case 9: return null;
+			case 10: return null;
+			case 11: return null;
+			case 12: return null;
+			case 13: return null;
+			case 14: return null;
+			case 15: return null;
+			case 17: return null;
+			case 18: return null;
+			case 19: return null;
+			case 20: return null;
+			case 21: return null;
+			case 22: return null;
+			case 23: return null;
+			case 25: return null;
+			case 26: return null;
+			case 27: return null;
+			case 28: return null;
+			case 29: return null;
+			case 30: return null;
+			case 35: return null;
+			case 40: return null;
+			case 44: return null;
+			case 43: return null;
+			case 45: return null;
+			case 50: return COLOR_DART_GREEN;
+			case 55: return null;
+			case 60: return COLOR_DART_GREEN;
+			case 61: return null;
+			case 62: return null;
+			case 63: return null;
+			case 64: return null;
+			case 65: return null;
+			case 70: return COLOR_DART_GREEN;
+			case 75: return null;
+			case 80: return COLOR_DART_GREEN;
+			case 81: return null;
+			case 82: return null;
+			case 83: return null;
+			case 84: return null;
+			case 85: return null;
+			case 86: return null;
+			//
+			case 101: return SCHOOL_BUS_COLOR;
+			case 102: return SCHOOL_BUS_COLOR;
+			case 104: return SCHOOL_BUS_COLOR;
+			case 117: return SCHOOL_BUS_COLOR;
+			case 150: return SCHOOL_BUS_COLOR;
+			case 151: return SCHOOL_BUS_COLOR;
+			case 152: return SCHOOL_BUS_COLOR;
+			case 153: return SCHOOL_BUS_COLOR;
+			case 180: return SCHOOL_BUS_COLOR;
+			//
+			case 305: return SCHOOL_BUS_COLOR;
+			case 311: return SCHOOL_BUS_COLOR;
+			case 314: return SCHOOL_BUS_COLOR;
+			case 315: return SCHOOL_BUS_COLOR;
+			case 316: return SCHOOL_BUS_COLOR;
+			case 322: return SCHOOL_BUS_COLOR;
+			case 325: return SCHOOL_BUS_COLOR;
+			case 331: return SCHOOL_BUS_COLOR;
+			case 332: return SCHOOL_BUS_COLOR;
+			case 333: return SCHOOL_BUS_COLOR;
+			case 334: return SCHOOL_BUS_COLOR;
+			case 335: return SCHOOL_BUS_COLOR;
+			case 336: return SCHOOL_BUS_COLOR;
+			case 338: return SCHOOL_BUS_COLOR;
+			case 339: return SCHOOL_BUS_COLOR;
+			case 341: return SCHOOL_BUS_COLOR;
+			case 342: return SCHOOL_BUS_COLOR;
+			case 346: return SCHOOL_BUS_COLOR;
+			case 348: return SCHOOL_BUS_COLOR;
+			case 349: return SCHOOL_BUS_COLOR;
+			case 352: return SCHOOL_BUS_COLOR;
+			case 354: return SCHOOL_BUS_COLOR;
+			case 356: return SCHOOL_BUS_COLOR;
+			case 358: return SCHOOL_BUS_COLOR;
+			//
+			case 514: return null;
+			case 523: return null;
+			//
+			case 808: return null;
+			//
+			case 1225: return null;
+			// @formatter:on
+			default:
+				throw new MTLog.Fatal("Unexpected route color %s!", gRoute.toStringPlus());
+			}
 		}
+		return super.getRouteColor(gRoute);
 	}
 
 	@NotNull
@@ -239,32 +205,21 @@ public class SaskatoonTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
-	}
-
-	@Override
 	public boolean directionFinderEnabled() {
 		return true;
 	}
 
-	private static final String FOREST_GROVE = "Forest Grv";
 	private static final Pattern FIX_FOREST_GROVE_ = Pattern.compile("((^|\\W)(forestgrove)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String FIX_FOREST_GROVE_REPLACEMENT = "$2" + FOREST_GROVE + "$4";
+	private static final String FIX_FOREST_GROVE_REPLACEMENT = "$2" + "Forest Grove" + "$4";
 
-	private static final String CITY = "City";
 	private static final Pattern FIX_CITY_ = Pattern.compile("((^|\\W)(ciity)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String FIX_CITY_REPLACEMENT = "$2" + CITY + "$4";
+	private static final String FIX_CITY_REPLACEMENT = "$2" + "City" + "$4";
 
 	private static final Pattern FIX_CENTRE_ = Pattern.compile("((^|\\W)(cenrtre)(\\W|$))", Pattern.CASE_INSENSITIVE);
 	private static final String FIX_CENTRE_REPLACEMENT = "$2" + "Centre" + "$4";
 
-	private static final String CONFEDERATION = "Confederation";
 	private static final Pattern FIX_CONFEDERATION_ = Pattern.compile("((^|\\W)(confederartion)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String FIX_CONFEDERATION_REPLACEMENT = "$2" + CONFEDERATION + "$4";
+	private static final String FIX_CONFEDERATION_REPLACEMENT = "$2" + "Confederation" + "$4";
 
 	@NotNull
 	@Override
